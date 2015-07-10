d44f7367eb51b8201f0391ad5eb5dfe47cdfb026
Paul-Émile MINY <paulemile.miny@gmail.com>
2015-07-10 14:54:33 +0200

    added ReferenceType::MEDIA

--- src/Formatter/TweetMessageFormatter.php
+++ src/Formatter/TweetMessageFormatter.php
@@ -16,9 +16,6 @@ class TweetMessageFormatter extends AbstractMessageFormatter
         $data = $reference->getData();
 
         switch($reference->getType()) {
-        case ReferenceType::URL:
-            return $this->createLinkString($data['expanded_url'], $data['display_url']);
-            break;
         case ReferenceType::USER:
             return $this->createLinkString('https://twitter.com/'. $data['screen_name'], $messagePart['text']);
             break;
@@ -26,6 +23,8 @@ class TweetMessageFormatter extends AbstractMessageFormatter
             return $this->createLinkString('https://twitter.com/hashtag/' . $data['text'], $messagePart['text']);
             break;
         case ReferenceType::VIDEO:
+        case ReferenceType::MEDIA:
+        case ReferenceType::URL:
             return $this->createLinkString($data['expanded_url'], $messagePart['display_url']);
             break;
         default:
--- src/Model/ReferenceType.php
+++ src/Model/ReferenceType.php
@@ -6,10 +6,11 @@ use CommerceGuys\Enum\AbstractEnum;
 
 class ReferenceType extends AbstractEnum
 {
-    const URL          = 'url';
-    const USER         = 'user';
-    const PAGE         = 'page';
-    const GROUP        = 'group';
-    const HASHTAG      = 'hashtag';
-    const VIDEO        = 'video';
+    const URL     = 'url';
+    const USER    = 'user';
+    const PAGE    = 'page';
+    const GROUP   = 'group';
+    const HASHTAG = 'hashtag';
+    const VIDEO   = 'video';
+    const MEDIA   = 'media';
 }
